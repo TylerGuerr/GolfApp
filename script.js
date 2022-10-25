c8b40ef8935db5bb673bc1f288fe98e761bf3e12
Tyler Guerricabeitia <hunets4506@gmail.com>
2022-10-25 09:01:51 -0600

    working api

--- script.js
+++ script.js
@@ -1,37 +1,118 @@
 let addButton = document.getElementById('addPlayer');
-function yardTotal() {
-    let yard1 = document.getElementById('yard1');
-    let yard2 = document.getElementById('yard2');
-    let yard3 = document.getElementById('yard3');
-    let yard4 = document.getElementById('yard4');
-    let yard5 = document.getElementById('yard5');
-    let yard6 = document.getElementById('yard6');
-    let yard7 = document.getElementById('yard7');
-    let yard8 = document.getElementById('yard8');
-    let yard9 = document.getElementById('yard9');
-    let yardTotal = document.getElementById('yardTotal');
+let playerNames = []
+// window.onload = initializeOnLoad();
 
-    let total = Number(yard1.value) + Number(yard2.value) + Number(yard3.value) + Number(yard4.value) + Number(yard5.value) + Number(yard6.value) +
-                Number(yard7.value) + Number(yard8.value) + Number(yard9.value);
-    let finalTotal = yardTotal.innerText = total;
-    yardTotal.parentElement = finalTotal;
-}
-function parTotal() {
-    let par1 = document.getElementById('par1');
-    let par2 = document.getElementById('par2');
-    let par3 = document.getElementById('par3');
-    let par4 = document.getElementById('par4');
-    let par5 = document.getElementById('par5');
-    let par6 = document.getElementById('par6');
-    let par7 = document.getElementById('par7');
-    let par8 = document.getElementById('par8');
-    let par9 = document.getElementById('par9');
-    let parTotal = document.getElementById('parTotal');
 
-    let total = Number(par1.value) + Number(par2.value) + Number(par3.value) + Number(par4.value) + Number(par5.value) + Number(par6.value) +
-                Number(par7.value) + Number(par8.value) + Number(par9.value);
-    parTotal.innerText = total;
+
+function foxSelected() {
+    const fox = document.getElementById('fox');
+    let golfTitle = document.getElementById('golfTitle')
+    golfTitle.innerText = fox.innerText;
+
+    window.onload = initializeOnLoad();
+    async function getCourse(id) {
+        const url = `https://golf-courses-api.herokuapp.com/courses/${id}`;
+        return fetch(url).then((response) => response.json());
+    }           
+    async function initializeOnLoad(id="18300"){
+        const data = await getCourse(id);
+        displayDataToPage(data);
+    }
+    function displayDataToPage(data){
+        console.log(data);
+        yardTotal(data);
+        parTotal(data);
+        handicapTotal(data);
+    }
 }
+function thanksSelected() {
+    const thanks = document.getElementById('thanks');
+    let golfTitle = document.getElementById('golfTitle')
+    golfTitle.innerText = thanks.innerText;
+
+    window.onload = initializeOnLoad();
+
+    async function getCourse(id) {
+        const url = `https://golf-courses-api.herokuapp.com/courses/${id}`;
+        return fetch(url).then((response) => response.json());
+    }           
+    async function initializeOnLoad(id="11819"){
+        const data = await getCourse(id);
+        displayDataToPage(data);
+    }
+    function displayDataToPage(data){
+        console.log(data);
+        yardTotal(data);
+        parTotal(data);
+        handicapTotal(data);
+    }
+}
+function spanishSelected() {
+    const spanish = document.getElementById('spanish');
+    let golfTitle = document.getElementById('golfTitle')
+    golfTitle.innerText = spanish.innerText;
+
+    window.onload = initializeOnLoad();
+
+    async function getCourse(id) {
+        const url = `https://golf-courses-api.herokuapp.com/courses/${id}`;
+        return fetch(url).then((response) => response.json());
+    }           
+    async function initializeOnLoad(id="19002"){
+        const data = await getCourse(id);
+        displayDataToPage(data);
+    }
+    function displayDataToPage(data){
+        console.log(data);
+        yardTotal(data);
+        parTotal(data);
+        handicapTotal(data);
+    }
+}
+function getName() {
+    let name = prompt("Enter a Name", "Name....");
+    if (name != null) {
+        document.getElementById('playerName').innerHTML = name;
+    } 
+    playerNames[0] = name;
+    console.log(playerNames);
+}
+
+function yardTotal(data) {
+    let yardNum = document.getElementsByClassName('yard');
+    let yardTotal = document.getElementById('yardTotal')
+    let yardTot = 0;
+    for (let x=0; x < 9; x++){
+        let yardsDataNum = data.data.holes[x].teeBoxes[0].yards;
+        yardTot += yardsDataNum;
+        yardNum[x].innerHTML = yardsDataNum;
+    }
+    yardTotal.innerHTML = `${yardTot}`;
+}
+function parTotal(data) {
+    let parNum = document.getElementsByClassName('par');
+    let parTotal = document.getElementById('parTotal')
+    let parTot = 0;
+    for (let x=0; x < 9; x++){
+        let parDataNum = data.data.holes[x].teeBoxes[0].par;
+        parTot += parDataNum;
+        parNum[x].innerHTML = parDataNum;
+    }
+    parTotal.innerHTML = `${parTot}`;
+}
+function handicapTotal(data) {
+    let handicapNum = document.getElementsByClassName('handicap');
+    let handicapTotal = document.getElementById('handicapTotal')
+    let handicapTot = 0;
+    for (let x=0; x < 9; x++){
+        let handicapDataNum = data.data.holes[x].teeBoxes[0].hcp;
+        handicapTot += handicapDataNum;
+        handicapNum[x].innerHTML = handicapDataNum;
+    }
+    handicapTotal.innerHTML = `${handicapTot}`;
+}
+
+
 function newPlayer() {
     let container = document.getElementById('container');
     let playerList = document.getElementById('playerList');
@@ -54,7 +135,7 @@ function newPlayer() {
         </thead>
         <tbody>
         <tr>
-            <th scope="row" id="title">Yardage</th>
+            <th scope="row" id="title">handicapage</th>
             <td><input maxlength="4" type="number" id="yard1"></input></td>
             <td><input maxlength="4" type="number" id="yard2"></input></td>
             <td><input maxlength="4" type="number" id="yard3"></input></td>
@@ -64,7 +145,7 @@ function newPlayer() {
             <td><input maxlength="4" type="number" id="yard7"></input></td>
             <td><input maxlength="4" type="number" id="yard8"></input></td>
             <td><input maxlength="4" type="number" id="yard9"></input></td>
-            <td id="yardTotal" onclick="yardTotal()">TOTAL</td>
+            <td id="yardTotal" onclick="yardTotal()">SUBMIT</td>
         </tr>
         <tr>
             <th scope="row" id="title">Par</th>
@@ -77,7 +158,7 @@ function newPlayer() {
             <td><input maxlength="4" type="number" id="par7"></input></td>
             <td><input maxlength="4" type="number" id="par8"></input></td>
             <td><input maxlength="4" type="number" id="par9"></input></td>
-            <td id="parTotal" onclick="parTotal()">TOTAL</td>
+            <td id="parTotal" onclick="parTotal()">SUBMIT</td>
         </tr>
         <tr>
             <th scope="row" id="title">Handicap</th>
@@ -112,17 +193,17 @@ function newPlayer() {
         </thead>
         <tbody>
         <tr>
-            <th scope="row" id="title">Mario</th>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-            <td id="totals">TOTAL</td>
+            <th scope="row" id="playerName" onclick="getName()">Enter Name...</th>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+            <td id="totals">SUBMIT</td>
         </tr>
         </tbody>
     </table>
