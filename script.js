b0114169c2997c3095414b91152fb72b362cc368
Tyler Guerricabeitia <hunets4506@gmail.com>
2022-10-26 08:02:19 -0600

    updated css and html

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let addButton = document.getElementById('addPlayer');
 let playerNames = []
-// window.onload = initializeOnLoad();
-
 
 
 function foxSelected() {
@@ -69,12 +67,37 @@ function spanishSelected() {
         handicapTotal(data);
     }
 }
-function getName() {
+
+function getNamePlayer1() {
     let name = prompt("Enter a Name", "Name....");
     if (name != null) {
-        document.getElementById('playerName').innerHTML = name;
+        document.getElementById('player1').innerHTML = name;
     } 
     playerNames[0] = name;
+    console.log(playerNames);
+}
+function getNamePlayer2() {
+    let name = prompt("Enter a Name", "Name....");
+    if (name != null) {
+        document.getElementById('player2').innerHTML = name;
+    } 
+    playerNames[1] = name;
+    console.log(playerNames);
+}
+function getNamePlayer3() {
+    let name = prompt("Enter a Name", "Name....");
+    if (name != null) {
+        document.getElementById('player3').innerHTML = name;
+    } 
+    playerNames[2] = name;
+    console.log(playerNames);
+}
+function getNamePlayer4() {
+    let name = prompt("Enter a Name", "Name....");
+    if (name != null) {
+        document.getElementById('player4').innerHTML = name;
+    } 
+    playerNames[3] = name;
     console.log(playerNames);
 }
 
@@ -110,102 +133,4 @@ function handicapTotal(data) {
         handicapNum[x].innerHTML = handicapDataNum;
     }
     handicapTotal.innerHTML = `${handicapTot}`;
-}
-
-
-function newPlayer() {
-    let container = document.getElementById('container');
-    let playerList = document.getElementById('playerList');
-    playerList.innerHTML += `<div class="container" id="container">
-    <table class="table">
-        <thead class="thead-dark">
-        <tr>
-            <th scope="col" id="title">HOLE</th>
-            <th scope="col">1</th>
-            <th scope="col">2</th>
-            <th scope="col">3</th>
-            <th scope="col">4</th>
-            <th scope="col">5</th>
-            <th scope="col">6</th>
-            <th scope="col">7</th>
-            <th scope="col">8</th>
-            <th scope="col">9</th>
-            <th scope="col" id="totals">OUT</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <th scope="row" id="title">handicapage</th>
-            <td><input maxlength="4" type="number" id="yard1"></input></td>
-            <td><input maxlength="4" type="number" id="yard2"></input></td>
-            <td><input maxlength="4" type="number" id="yard3"></input></td>
-            <td><input maxlength="4" type="number" id="yard4"></input></td>
-            <td><input maxlength="4" type="number" id="yard5"></input></td>
-            <td><input maxlength="4" type="number" id="yard6"></input></td>
-            <td><input maxlength="4" type="number" id="yard7"></input></td>
-            <td><input maxlength="4" type="number" id="yard8"></input></td>
-            <td><input maxlength="4" type="number" id="yard9"></input></td>
-            <td id="yardTotal" onclick="yardTotal()">SUBMIT</td>
-        </tr>
-        <tr>
-            <th scope="row" id="title">Par</th>
-            <td><input maxlength="4" type="number" id="par1"></input></td>
-            <td><input maxlength="4" type="number" id="par2"></input></td>
-            <td><input maxlength="4" type="number" id="par3"></input></td>
-            <td><input maxlength="4" type="number" id="par4"></input></td>
-            <td><input maxlength="4" type="number" id="par5"></input></td>
-            <td><input maxlength="4" type="number" id="par6"></input></td>
-            <td><input maxlength="4" type="number" id="par7"></input></td>
-            <td><input maxlength="4" type="number" id="par8"></input></td>
-            <td><input maxlength="4" type="number" id="par9"></input></td>
-            <td id="parTotal" onclick="parTotal()">SUBMIT</td>
-        </tr>
-        <tr>
-            <th scope="row" id="title">Handicap</th>
-            <td><input maxlength="4" type="number" id="handicap1"></input></td>
-            <td><input maxlength="4" type="number" id="handicap2"></input></td>
-            <td><input maxlength="4" type="number" id="handicap3"></input></td>
-            <td><input maxlength="4" type="number" id="handicap4"></input></td>
-            <td><input maxlength="4" type="number" id="handicap5"></input></td>
-            <td><input maxlength="4" type="number" id="handicap6"></input></td>
-            <td><input maxlength="4" type="number" id="handicap7"></input></td>
-            <td><input maxlength="4" type="number" id="handicap8"></input></td>
-            <td><input maxlength="4" type="number" id="handicap9"></input></td>
-            <td></td>
-        </tr>
-        </tbody>
-    </table>   
-    <table class="table">
-        <thead class="thead-light">
-        <tr>
-            <th scope="col" id="title">PLAYER</th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-            <th scope="col" id="totals">SCORE</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <th scope="row" id="playerName" onclick="getName()">Enter Name...</th>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>4</td>
-                <td>5</td>
-                <td>6</td>
-                <td>7</td>
-                <td>8</td>
-                <td>9</td>
-            <td id="totals">SUBMIT</td>
-        </tr>
-        </tbody>
-    </table>
-</div>`
 }
