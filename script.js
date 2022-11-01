be1f0c90a142f5dca6ba8de232b13ff0def624a5
Tyler Guerricabeitia <hunets4506@gmail.com>
2022-11-01 10:22:53 -0600

    working player scores and updated objects

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 let addButton = document.getElementById('addPlayer');
-let playerNames = []
+let playerNames = {
+    0: {
+        name:'',
+        id:0,
+        score:''
+    },
+    1: {
+        name:'',
+        id:1,
+        score:''
+    },
+    2: {
+        name:'',
+        id:2,
+        score:''
+    },
+    3: {
+        name:'',
+        id:3,
+        score:''
+    }
+}
 
 
 function foxSelected() {
@@ -17,7 +38,6 @@ function foxSelected() {
         displayDataToPage(data);
     }
     function displayDataToPage(data){
-        console.log(data);
         yardTotal(data);
         parTotal(data);
         handicapTotal(data);
@@ -39,7 +59,6 @@ function thanksSelected() {
         displayDataToPage(data);
     }
     function displayDataToPage(data){
-        console.log(data);
         yardTotal(data);
         parTotal(data);
         handicapTotal(data);
@@ -61,7 +80,6 @@ function spanishSelected() {
         displayDataToPage(data);
     }
     function displayDataToPage(data){
-        console.log(data);
         yardTotal(data);
         parTotal(data);
         handicapTotal(data);
@@ -74,7 +92,7 @@ function getNamePlayer1() {
     if (name != null) {
         document.getElementById('player1').innerHTML = name;
     } 
-    playerNames[0] = name;
+    playerNames[0].name = name;
     card2Name.innerText = name;
     console.log(playerNames);
 }
@@ -84,7 +102,7 @@ function getNamePlayer2() {
     if (name != null) {
         document.getElementById('player2').innerHTML = name;
     } 
-    playerNames[1] = name;
+    playerNames[1].name = name;
     card2Name.innerText = name;
     console.log(playerNames);
 }
@@ -94,7 +112,7 @@ function getNamePlayer3() {
     if (name != null) {
         document.getElementById('player3').innerHTML = name;
     } 
-    playerNames[2] = name;
+    playerNames[2].name = name;
     card2Name.innerText = name;
     console.log(playerNames);
 }
@@ -104,19 +122,21 @@ function getNamePlayer4() {
     if (name != null) {
         document.getElementById('player4').innerHTML = name;
     } 
-    playerNames[3] = name;
+    playerNames[3].name = name;
     card2Name.innerText = name;
     console.log(playerNames);
 }
 
 function yardTotal(data) {
     let yardNum = document.getElementsByClassName('yard');
+    let firstYardTotal = document.getElementById('yardTotal');
     let yard2Total = document.getElementById('yard2total');
     let yardTot = 0;
     for (let x=0; x < 9; x++){
         let yardsDataNum = data.data.holes[x].teeBoxes[0].yards;
         yardTot += yardsDataNum;
         yardNum[x].innerHTML = yardsDataNum;
+        firstYardTotal.innerHTML = `${yardTot}`;
         if (x >= 8) {
             for(let x=9; x<18; x++) {
                 let yardsDataNum = data.data.holes[x].teeBoxes[0].yards;
@@ -130,12 +150,14 @@ function yardTotal(data) {
 }
 function parTotal(data) {
     let parNum = document.getElementsByClassName('par');
+    let firstParTotal = document.getElementById('parTotal');
     let par2total = document.getElementById('par2total')
     let parTot = 0;
     for (let x=0; x < 9; x++){
         let parDataNum = data.data.holes[x].teeBoxes[0].par;
         parTot += parDataNum;
         parNum[x].innerHTML = parDataNum;
+        firstParTotal.innerHTML = `${parTot}`;
         if (x >=8) {
             for (let x=9; x<18; x++){
                 let parDataNum = data.data.holes[x].teeBoxes[0].par;
@@ -149,12 +171,14 @@ function parTotal(data) {
 }
 function handicapTotal(data) {
     let handicapNum = document.getElementsByClassName('handicap');
+    let firstHandicapTotal = document.getElementById('handicapTotal');
     let handicap2total = document.getElementById('handicap2total')
     let handicapTot = 0;
     for (let x=0; x < 9; x++){
         let handicapDataNum = data.data.holes[x].teeBoxes[0].hcp;
         handicapTot += handicapDataNum;
         handicapNum[x].innerHTML = handicapDataNum;
+        firstHandicapTotal.innerHTML = `${handicapTot}`;
         if(x >= 8) {
             for(let x=9; x<18; x++){
                 let handicapDataNum = data.data.holes[x].teeBoxes[0].hcp;
@@ -186,10 +210,16 @@ function getPlayer1score(){
     let hole17 = document.getElementById('p1card2hole17score').value;
     let hole18 = document.getElementById('p1card2hole18score').value;
     let card2player1total = document.getElementById('card2player1total');
+    let handicap2Total = document.getElementById('handicap2total').innerHTML;
 
-    let total = Number(hole1) + Number(hole2) + Number(hole3) + Number(hole4) + Number(hole5) + Number(hole6) + Number(hole7) + Number(hole8) + Number(hole9) + Number(hole10) + Number(hole11) + Number(hole12) + Number(hole13) + Number(hole14) + Number(hole15) + Number(hole16) + Number(hole17) + Number(hole18);
+    let total2 = Number(hole1) + Number(hole2) + Number(hole3) + Number(hole4) + Number(hole5) + Number(hole6) + Number(hole7) + Number(hole8) + Number(hole9) + Number(hole10) + Number(hole11) + Number(hole12) + Number(hole13) + Number(hole14) + Number(hole15) + Number(hole16) + Number(hole17) + Number(hole18);
 
-    card2player1total.innerText = total;
+    handicap2Total = Number(handicap2Total);
+
+    let final2 = total2 - handicap2Total;
+
+    card2player1total.innerText = final2;
+    playerNames[0].score = final2;
 }
 function getPlayer2score(){
     let hole1 = document.getElementById('p2hole1score').value;
@@ -211,10 +241,15 @@ function getPlayer2score(){
     let hole17 = document.getElementById('p2card2hole17score').value;
     let hole18 = document.getElementById('p2card2hole18score').value;
     let card2player2total = document.getElementById('card2player2total');
+    let handicap2Total = document.getElementById('handicap2total').innerHTML;
 
     let total = Number(hole1) + Number(hole2) + Number(hole3) + Number(hole4) + Number(hole5) + Number(hole6) + Number(hole7) + Number(hole8) + Number(hole9) + Number(hole10) + Number(hole11) + Number(hole12) + Number(hole13) + Number(hole14) + Number(hole15) + Number(hole16) + Number(hole17) + Number(hole18);
 
-    card2player2total.innerText = total;
+    handicap2Total = Number(handicap2Total);
+    let final = total - handicap2Total;
+
+    card2player2total.innerText = final;
+    playerNames[1].score = final;
 }
 function getPlayer3score(){
     let hole1 = document.getElementById('p3hole1score').value;
@@ -236,10 +271,15 @@ function getPlayer3score(){
     let hole17 = document.getElementById('p3card2hole17score').value;
     let hole18 = document.getElementById('p3card2hole18score').value;
     let card2player3total = document.getElementById('card2player3total');
+    let handicap2Total = document.getElementById('handicap2total').innerHTML;
 
     let total = Number(hole1) + Number(hole2) + Number(hole3) + Number(hole4) + Number(hole5) + Number(hole6) + Number(hole7) + Number(hole8) + Number(hole9) + Number(hole10) + Number(hole11) + Number(hole12) + Number(hole13) + Number(hole14) + Number(hole15) + Number(hole16) + Number(hole17) + Number(hole18);
 
-    card2player3total.innerText = total;
+    handicap2Total = Number(handicap2Total);
+    let final = total - handicap2Total;
+
+    card2player3total.innerText = final;
+    playerNames[2].score = final;
 }
 function getPlayer4score(){
     let hole1 = document.getElementById('p4hole1score').value;
@@ -261,8 +301,13 @@ function getPlayer4score(){
     let hole17 = document.getElementById('p3card2hole17score').value;
     let hole18 = document.getElementById('p3card2hole18score').value;
     let card2player4total = document.getElementById('card2player4total');
+    let handicap2Total = document.getElementById('handicap2total').innerHTML;
 
     let total = Number(hole1) + Number(hole2) + Number(hole3) + Number(hole4) + Number(hole5) + Number(hole6) + Number(hole7) + Number(hole8) + Number(hole9) + Number(hole10) + Number(hole11) + Number(hole12) + Number(hole13) + Number(hole14) + Number(hole15) + Number(hole16) + Number(hole17) + Number(hole18);
 
-    card2player4total.innerText = total;
+    handicap2Total = Number(handicap2Total);
+    let final = total - handicap2Total;
+
+    card2player4total.innerText = final;
+    playerNames[3].score = final;
 }
